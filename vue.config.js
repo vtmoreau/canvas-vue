65bee33c06e04f9be72cde5e44215d9e59677c44
Vincent Moreau <vincent.moreau@protonmail.com>
2022-11-21 23:07:36 +0100

    add config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/canvas-vue/'
+  publicPath: '/canvas-vue/src'
 })
